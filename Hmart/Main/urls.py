e2870b3e448fa55226199aa31e99aadd995e2e00
Igor Zaitsev <65121090+igor-zaitsau@users.noreply.github.com>
2022-11-20 14:29:29 +0300

    add all-products page

--- Hmart/Main/urls.py
+++ Hmart/Main/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('products/', all_products, name='all_products'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('product/', single_product, name='single_product')
--- Hmart/Main/views.py
+++ Hmart/Main/views.py
@@ -2,16 +2,19 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'Main/home.html')
+    return render(request, 'Main/home.html', {'title': 'Главная страница'})
+
+def all_products(request):
+    return render(request, 'Main/all-products.html', {'title': 'Все товары'})
 
 def single_product(request):
     return render(request, 'Main/single-product.html')
 
 def about(request):
-    return render(request, 'Main/about.html')
+    return render(request, 'Main/about.html', {'title': 'О нас'})
 
 def contact(request):
-    return render(request, 'Main/contact.html')
+    return render(request, 'Main/contact.html', {'title': 'Контакты'})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("Error 404")
