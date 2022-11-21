fe6b3550fa9c252cf7b42cc8bcb0669ebf9a52de
Igor Zaitsev <65121090+igor-zaitsau@users.noreply.github.com>
2022-11-21 09:03:14 +0300

    --

--- Hmart/users/urls.py
+++ Hmart/users/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import *
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('registration', registration, name='registration'),
     path('cart/', cart, name='cart'),
     path('wishlist/', wishlist, name='wishlist'),
 ]
--- Hmart/users/views.py
+++ Hmart/users/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 def login(request):
     return render(request, 'users/login.html')
+
+def registration(request):
+    return render(request, 'users/registration.html')
 
 def logout(request):
     pass
